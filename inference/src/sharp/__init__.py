1ea02cc3b73b0bf63bfc9756725e32233e001d78
xuejian <xuejianqqh1@outlook.com>
2026-06-06 22:07:12 +0800

    fix: resolve file encoding corruption caused by powershell script

--- inference/src/sharp/__init__.py
+++
@@ -1,4 +0,0 @@
-"""For licensing see accompanying LICENSE file.
-
-Copyright (c) 2025 Moment3D Contributors.
-"""
--- inference/src/sharp/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Contains utils packages.
-
-For licensing see accompanying LICENSE file.
-Copyright (c) 2025 Moment3D Contributors.
-"""
